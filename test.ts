314e4af48a30f6f759a310005b8bff09dbb6896f
shobhan dash <28.11.shobhan.dash@gmail.com>
2024-02-10 22:58:30 +0530

    [FIX]: commas within value not treated as csv delimiters

--- test.ts
+++ test.ts
@@ -5,17 +5,17 @@ import excelRemastered from './src/index';
 const data = {
     
   "page": 1,
-  "total_results": 10000,
+  "total_results": "10,000",
   "total_pages": 500,
   "results": []
 }
-const csvData = excelRemastered.jsonToCsv(data);
+const csvData = excelRemastered.jsonToCsv(data, 'src/jsonToCsv/output.csv');
 
 // Giving the second optional parameter will write the CSV to the file path provided
-const download = excelRemastered.jsonToCsv('src/jsonToCsv/exampleData/example-testing.json', 'src/jsonToCsv/output.csv');
+// const download = excelRemastered.jsonToCsv('src/jsonToCsv/exampleData/example-testing.json', 'src/jsonToCsv/output.csv');
 
 console.log('====================================');
 console.log(csvData);
 console.log('====================================');
-console.log(download);
+// console.log(download);
 console.log('====================================');
